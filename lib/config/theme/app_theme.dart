29f5fd760682b87d9c67f382ad232ac3df7ee4e0
Cristian <cristianjbc99@gmail.com>
2024-03-31 12:19:11 -0600

    Tema y estilos

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.purple,
+];
+
+class AppTheme {
+  final int selectColor;
+
+  AppTheme({required this.selectColor})
+      : assert(selectColor >= 0, 'Select color must be greater than 0'),
+        assert(selectColor < colorList.length, 'Select color must be less than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+        //useMaterial3: true,
+        colorSchemeSeed: colorList[selectColor],
+      );
+}
