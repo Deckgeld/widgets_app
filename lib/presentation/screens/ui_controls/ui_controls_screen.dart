06b1176ed0e87fab1f6db25ebb9df073757be35e
Cristian <cristianjbc99@gmail.com>
2024-04-02 15:32:44 -0600

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControllScreen extends StatelessWidget {
+  static const String name = 'ui_controls_screen';
+  const UiControllScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
