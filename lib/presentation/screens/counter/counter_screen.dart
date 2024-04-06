0fdc407189fc229c3dccb222dd8c3ab3672c15e9
Cristian <cristianjbc99@gmail.com>
2024-04-05 18:19:52 -0600

    counter_screen with riverpod

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final counter = 0;
+  //Agregar el parametro ref, para poder acceder a los providers
+  Widget build(BuildContext context, WidgetRef ref) {
+    final clickCounter = ref.watch(counterProvider);
+
+    
 
     return Scaffold(
         appBar: AppBar(
           title: const Text('Counter Screen'),
         ),
         body: Center(
-          child: Text('Valor $counter', style: Theme.of(context).textTheme.titleLarge,),
+          child: Text('Valor $clickCounter', style: Theme.of(context).textTheme.titleLarge,),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            //Caulquier de las dos formas es valida
+            //Dentro de una funcion no se puede usar ref.watch ya que no es un widget
+            ref.read(counterProvider.notifier).state++;
+            // ref.read(counterProvider.notifier).update((state) => state + 1);
+          },
           child: const Icon(Icons.add),
         )
     );
